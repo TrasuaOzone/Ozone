af5506dfc141618e0bb22d0b5fafd8af93297211
TrasuaOzone <vanyen0thixtomo@gmail.com>
2025-08-27 14:24:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,14 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("odaycogi-cache-v1").then(cache => {
       return cache.addAll([
-        "/index.html",
-        "/manifest.json",
-        "/images/icon-192.png",
-        "/images/icon-512.png",
-        "/style.css",
-        "/dich-vu.html",
-        "/trasua.html"
+        "index.html",
+        "manifest.json",
+        "images/icon-192.png",
+        "images/icon-512.png",
+        "images/LG.png",
+        "trasua.html",
+        "dich-vu.html",
+        "dang-ky.html"
       ]);
     })
   );
